bd1abef0dc91fd4907eb6db931744ba8d4bd6e8c
Yuzuki-9 <s1f102000624@iniad.org>
2021-09-18 17:17:57 +0900

    validation

--- main.py
+++ main.py
@@ -105,7 +105,16 @@ async def create_item(item_id: int, item: Item, q: Optional[str] = None):
         result.update({"q": q})
     return result
 
-
+# クエリパラメータと文字列の検証
+# バリデーション・正規表現
+@app.get("/items/")
+async def read_items(
+    q: Optional[str] = Query(None, min_length=3, max_length=50, regex="^fixedquery$")  # 必須にするには「None」→「...」
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q":q})
+    return results
 
 
 # class Data(BaseModel):
